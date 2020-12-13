a30d781f2968e9715afa623487507de564ff1f19
Júnior Oliveira <juninsoader@gmail.com>
2020-12-12 22:34:54 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Board board = new Board(8, 8);
     }
 }
